61f25f6aaa8c4a7b3227a87340c70accfa5bc5dc
Luan <luanmarcosdev@gmail.com>
2024-04-19 09:23:04 -0300

    create MVVM-C project

---
+++ MVVM-C/MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Luan Arruda on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
