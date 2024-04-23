ce262bfb8b68551722bb2ddf7f252a5a9b065318
Luan <luanmarcosdev@gmail.com>
2024-04-23 13:04:25 -0300

    organizing project

---
+++ MVVM-C/MVVM-C/ViewModels/UserViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserViewModel.swift
+//  MVVM-C
+//
+//  Created by Luan Arruda on 23/04/24.
+//
+
+import Foundation
+
+class UserViewModel {
+    
+    // Comunica com o Model
+    
+    // exibe e trata os dados vindo dos Models
+    
+    // Cuida das regras do negocio do app
+    
+    // Faz as chamadas da API
+    
+}
